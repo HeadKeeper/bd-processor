4d194b8f1845d8979c3d441ffb835f4cc43e68c8
max <maks1464@gmail.com>
2018-10-27 17:22:45 +0300

    Extend "Question" struct

--- db/entity.go
+++ db/entity.go
@@ -2,18 +2,22 @@ package db
 
 import "gopkg.in/mgo.v2/bson"
 
+type QuestionBatch struct {
+	Items []Question `json:"items"`
+}
+
 type Question struct {
-	Id               bson.ObjectId `bson:"_id"`
-	Tags             []string      `bson:"tags"`
-	IsAnswered       bool          `bson:"is_answered"`
-	ViewCount        int           `bson:"view_count"`
-	AcceptedAnswerId int           `bson:"accepted_answer_id"`
-	AnswerCount      int           `bson:"answer_count"`
-	Score            int           `bson:"score"`
-	LastActivityDate int64         `bson:"last_activity_date"`
-	CreationDate     int64         `bson:"creation_date"`
-	LastEditDate     int64         `bson:"last_edit_date"`
-	QuestionId       int           `bson:"question_id"`
-	Link             string        `bson:"link"`
-	Title            string        `bson:"title"`
+	Id               bson.ObjectId `bson:"_id" json:"id"`
+	Tags             []string      `bson:"tags" json:"tags"`
+	IsAnswered       bool          `bson:"is_answered" json:"is_answered"`
+	ViewCount        int           `bson:"view_count" json:"view_count"`
+	AcceptedAnswerId int           `bson:"accepted_answer_id" json:"accepted_answer_id"`
+	AnswerCount      int           `bson:"answer_count" json:"answer_count"`
+	Score            int           `bson:"score" json:"score"`
+	LastActivityDate uint64        `bson:"last_activity_date" json:"last_activity_date"`
+	CreationDate     uint64        `bson:"creation_date" json:"creation_date"`
+	LastEditDate     uint64        `bson:"last_edit_date" json:"last_edit_date"`
+	QuestionId       int           `bson:"question_id" json:"question_id"`
+	Link             string        `bson:"link" json:"link"`
+	Title            string        `bson:"title" json:"title"`
 }
