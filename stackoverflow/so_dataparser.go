10448212cd862caa2efd07a33fddcffafe2ce489
max <maks1464@gmail.com>
2018-11-05 00:53:00 +0300

    Added statistics with CoTags

--- stackoverflow/so_dataparser.go
+++ stackoverflow/so_dataparser.go
@@ -34,8 +34,50 @@ func parseCoTags(connection db.DbConnection, collectionName string, statistics *
 	questions, _ := connection.FindByQuery(collectionName, nil)
 	var parseGroup sync.WaitGroup
 	parseGroup.Add(1)
-	go collectCoTags(statistics, questions.Items, &parseGroup)
+	//go collectCoTags(statistics, questions.Items, &parseGroup)
+	//go collectViewByCoTags(statistics, questions.Items, &parseGroup)
+	//go collectAnswersCountByCoTags(statistics, questions.Items, &parseGroup)
+	go collectTotalScoreByCoTag(statistics, questions.Items, &parseGroup)
 	parseGroup.Wait()
+}
+
+func collectTotalScoreByCoTag(statistics *SO_Statistics, questions []db.Question, group *sync.WaitGroup) {
+	coTags := make(map[string]int)
+	for index, question := range questions {
+		fmt.Printf("CO-TAG (Total Score): #%d is started...\n", index)
+		for _, tag := range question.Tags {
+			coTags[tag] += question.Score
+		}
+	}
+	statistics.TotalScoreByCoTag = getSortedMap(coTags, 16)
+	fmt.Println(statistics.TotalScoreByCoTag)
+	group.Done()
+}
+
+func collectAnswersCountByCoTags(statistics *SO_Statistics, questions []db.Question, group *sync.WaitGroup) {
+	coTags := make(map[string]int)
+	for index, question := range questions {
+		fmt.Printf("CO-TAG (Answers Count): #%d is started...\n", index)
+		for _, tag := range question.Tags {
+			coTags[tag] += question.AnswerCount
+		}
+	}
+	statistics.AnswersCountByCoTag = getSortedMap(coTags, 16)
+	fmt.Println(statistics.AnswersCountByCoTag)
+	group.Done()
+}
+
+func collectViewByCoTags(statistics *SO_Statistics, questions []db.Question, group *sync.WaitGroup) {
+	coTags := make(map[string]int)
+	for index, question := range questions {
+		fmt.Printf("CO-TAG (View Count): #%d is started...\n", index)
+		for _, tag := range question.Tags {
+			coTags[tag] += question.ViewCount
+		}
+	}
+	statistics.ViewCountByCoTag = getSortedMap(coTags, 16)
+	fmt.Println(statistics.ViewCountByCoTag)
+	group.Done()
 }
 
 func collectCoTags(statistics *SO_Statistics, questions []db.Question, group *sync.WaitGroup) {
@@ -46,7 +88,7 @@ func collectCoTags(statistics *SO_Statistics, questions []db.Question, group *sy
 			coTags[tag]++
 		}
 	}
-	statistics.CoTags = getSortedMap(coTags, 10)
+	statistics.CoTags = getSortedMap(coTags, 16) // Top 15, excluded tag herself
 	fmt.Println(statistics.CoTags)
 	group.Done()
 }
