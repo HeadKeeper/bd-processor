a68d6ab71f892554b06b324fc0c15f24f3006e6c
max <maks1464@gmail.com>
2018-11-04 22:29:50 +0300

    Added SO_Statistics entity

--- stackoverflow/so_statistics.go
+++ stackoverflow/so_statistics.go
@@ -1,1 +1,12 @@
 package stackoverflow
+
+type SO_Statistics struct {
+	Tag                 string
+	AnsweredPercent     float64
+	CoTags              map[string]int
+	QuestionsByYear     map[string]int
+	AnswersByYear       map[string]float64
+	ViewCountByCoTag    map[string]int
+	AnswersCountByCoTag map[string]int
+	TotalScoreByCoTag   map[string]int
+}
